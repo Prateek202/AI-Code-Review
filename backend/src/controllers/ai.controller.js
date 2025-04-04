8e933ca77f1ae5a8ea079aef702ce46c97fa1282
Prateek Thakur <prateek.2022ca069@mnnit.ac.in>
2025-04-05 01:12:27 +0530

    completed

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,13 +1,17 @@
-const aiService = require("../services/ai.service");
+const aiService = require("../services/ai.service")
 
-module.exports.getReview = async (res,req)=>{
+
+module.exports.getReview = async (req, res) => {
+
     const code = req.body.code;
 
-    if(!code) {
-        return res.status(400).json({ error: 'code is required' });
+    if (!code) {
+        return res.status(400).send("Prompt is required");
     }
 
     const response = await aiService(code);
 
+
     res.send(response);
+
 }
